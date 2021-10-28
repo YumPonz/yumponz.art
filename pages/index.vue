fba10e39eb4272d2003a122e6fb0c7dfd84254df
ikoko125 <68625796+ikoko125@users.noreply.github.com>
2021-10-28 12:02:05 +0900

    font
    title

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
 <div>
     <main>
-        aaa
+        <h1>美味ぽん酢サーバー</h1>
     </main>
 </div>
 
 </template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Kaisei+Opti:wght@700&display=swap');
+main{
+    font-family: 'Kaisei Opti', serif;
+}
+</style>
